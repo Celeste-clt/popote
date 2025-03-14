36d1b6816dd070de9570cad82f3fe23f75a5b246
Celeste <celeste.colletti@icloud.com>
2025-03-14 16:35:09 +0100

    erreur 404 + menu .php

--- page.php
+++ page.php
@@ -1,3 +1,17 @@
 <?php
+echo"<headre>";
+echo"<a href='http://localhost/popote-1/menu.php'>menu</a>";
+echo "</header>";
+echo "<h1>Popote minute</h1>";
+
+
+$page = "./popote-1". $_GET ['pages'].'.php';
+
+if(file_exists($page)){
+    include ( $_GET['pages'] . ".php");
+} else{
+    echo "page introuvable erreur 404 $page";
+}
+
 
 ?>
